345bc23a304e203769501ca3d0bec351fd2eedfe
LancelotCR <lancelot.clement.ramis@gmail.com>
2025-05-15 11:42:09 +0200

    non terminé reste erreur

--- Déformée du cable.py
+++ Déformée du cable.py
@@ -14,11 +14,19 @@ rho_eau = 1025  # kg/m³
 # --- CÂBLE ---
 L_tot = 50.0       # Longueur totale du câble (m)
 n = 150            # Nombre de segments
-D = 0.0063         # Diamètre du câble (m)
-S = (D**2) * np.pi / 4 # section du cable
-mass_lin = 0.5     # Masse linéique du câble (kg/m)
-v_kt = 3          # Vitesse en noeud
+delta_s = L_tot / n# Longueur d'un segement
+
+D = 0.0063              # Diamètre du câble (m)
+S = (D**2) * np.pi / 4  # section du cable
+mass_lin = 0.02          # Masse linéique du câble (kg/m)
+
+P_c = - (mass_lin - rho_eau * (np.pi * D ** 2 / 4)) * g * delta_s  # Poids apparent
+
+# --- bateau ---
+v_kt = 3                  # Vitesse en noeud
 v_bateau = v_kt * 0.5144  # Vitesse en m/s
+
+
 
 # --- SONAR ---
 m_sondeur_imm = 6.7             # Masse apparente (plongée)
@@ -27,9 +35,10 @@ S_sondeur = (D_sondeur**2) * np.pi / 4  # Surface frontale
 
 
 # --- COEFFICIENTS HYDRODYNAMIQUES ---
-Cd_sondeur = 0.7
-Cd = 1.2     # Traînée normale
-Cf = 0.01    # Traînée tangentielle
+Cd_sondeur = 0.7 # Trainée tangentielle sondeur
+
+Cd = 1.2     # Traînée normale section cable
+Cf = 0.01    # Traînée tangentielle section cable
 
 # --- 4. FORCES ----
 
@@ -41,48 +50,97 @@ def calcul_forces(theta):
 
     F_n = - 0.5 * rho_eau * v_n**2 * Cd * D * delta_s
     F_t = - 0.5 * rho_eau * v_t**2 * Cf * S
-    P = - (mass_lin - rho_eau * (np.pi * D**2 / 4)) * g * delta_s  # Poids apparent
 
-
-
-    return F_n, F_t, P
+    return F_n, F_t
 
 def effort_sondeur():
     Fn = m_sondeur_imm * g
     Ft = 0.5 * rho_eau * Cd_sondeur * S_sondeur * v_bateau**2
-    return Fn, Ft
-print(effort_sondeur(), np+arctan(effort_sondeur()[0], effort_sondeur()[1]))
-
+    return (-Ft, -Fn)
+print(effort_sondeur(), np.arctan(effort_sondeur()[0]/effort_sondeur()[1]))
 
 
 # --- 5. CALCUL DE LA DÉFORMÉE ---
 
 def resoudre_deformee():
-    theta = np.zeros(n + 1)
+    alpha = np.zeros(n)
+    #Beta = alpha[i+1]
     x = np.zeros(n + 1)
     z = np.zeros(n + 1)
+    F = []
+
     eff_sond = (effort_sondeur())
-    theta[0] = np.arctan(eff_sond[0], eff_sond[1])
-    delta_s = L_tot / n
+    F.append(eff_sond)
+    x[0] = 0
+    z[0] = 0
 
-    F = [(eff_sond)]
 
-    F_n, F_t, P = calcul_forces(theta[0])
-    Fx = F_n * np.cos(theta[0]) - F_t * np.sin(theta[0])
-    Fz = -P + F_n * np.sin(theta[0]) + F_t * np.cos(theta[0])
-    F.append()
-    F = (Fx, )
+    ## on initialise l'angle de la première section
+    alpha[0] = np.arctan(eff_sond[0]/eff_sond[1])
+
+    x[1] = x[0] + delta_s * np.cos(alpha[0])
+    z[1] = z[0] + delta_s * np.sin(alpha[0])
+
+    F_nc, F_tc = calcul_forces(alpha[0])
+
+    #region == calcul ==
+        #projection sur la normal du câble :
+    # -F_nc + np.sin(alpha[1]) * F[1] - np.sin(alpha[0]) * (F[0] - P_c) = 0
+
+        #projection sur la tengente du câble :
+    # -F_tc + np.cos(alpha[1]) * F[1] - np.cos(alpha[0]) * (F[0] + P_c) = 0
+
+    # (np.sin(alpha[1]) * F[1])²  = (F_nc + np.sin(alpha[0]) * (F[0] - P_c))²
+    # (np.cos(alpha[1]) * F[1])²  = (F_nc + np.cos(alpha[0]) * (F[0] + P_c))²
+
+    # ( (np.sin(alpha[1])² + (np.cos(alpha[1])² ) * 2 x F[1]²
+    #        = (F_nc + np.sin(alpha[0]) * (F[0] - P_c))² + (F_nc + np.cos(alpha[0]) * (F[0] + P_c))²
+
+    # F[1] = rac (  1/2 x [ (F_nc + np.sin(alpha[0]) * (F[0] - P_c))² + (F_nc + np.cos(alpha[0]) * (F[0] + P_c))² ] )
+    # alpha[1] = arcsin(-(1 / F[1]) * (F_nc + sin(-alpha[0]) * (F[0] + P_c)))
+    #endregion
+
+    F_i = (  np.sqrt((1/2)*(
+                   (F_nc
+                    +  np.sin(alpha[0]) * (F[0][0] + P_c)
+                    )**2
+                    +
+                   (F_tc
+                    + np.cos(alpha[0]) * (F[0][0] + P_c)
+                    )**2
+                    ))
+            )
+
+    alpha[1]  = np.arcsin(-(1/F_i)*(F_nc  + np.sin(-alpha[0]) * (F[0][0] + P_c)))
+
+    F.append( (F_i * np.sin(alpha[1]), F_i * np.cos(alpha[1])) )
+
+
+
 
     for i in range(n):
-        F_n, F_t, P = calcul_forces(theta[i])
-        Fx = F_n * np.cos(theta[i]) - F_t * np.sin(theta[i])
-        Fz = -P + F_n * np.sin(theta[i]) + F_t * np.cos(theta[i])
+        x[i+1] = x[i] + delta_s * np.cos(alpha[i])
+        z[i+1] = z[i] + delta_s * np.sin(alpha[i])
 
+        F_nc, F_tc = calcul_forces(alpha[i])
 
+        F_imoins1 = np.sqrt(F[i][0]**2 + F[i][1]**2)
 
-        theta[i+1] = np.arctan(Fz/Fx)
-        x[i] = x[i] + delta_s * np.cos(theta[i+1])
-        z[i] = z[i] + delta_s * np.sin(theta[i+1])
+        F_i = (np.sqrt((1 / 2) * (
+                (F_nc
+                 + np.sin(alpha[i]) * (F_imoins1 + P_c)
+                 ) ** 2
+                +
+                (F_tc
+                 + np.cos(alpha[i]) * (F_imoins1 + P_c)
+                 ) ** 2
+        ))
+               )
+
+        # on actualise l'angle et on ajoute la positionn
+        alpha[i+1] = np.arcsin(-(1 / F_i) * (F_nc + np.sin(-alpha[i]) * (F_imoins1 + P_c)))
+
+        F.append((F_i * np.sin(alpha[i+1]), F_i * np.cos(alpha[i+1])))
 
     return x, z
 
@@ -91,7 +149,7 @@ def resoudre_deformee():
 
 def tracer_deformee(x, z):
     plt.figure(figsize=(10, 6))
-    plt.plot(x, -z)  # z orienté vers le bas
+    plt.plot(x, z)  # z orienté vers le bas
     plt.xlabel("Distance horizontale (m)")
     plt.ylabel("Profondeur (m)")
     plt.title("Déformée du câble")
